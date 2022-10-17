11429cdd9acc00aa38534b8e264461fa48a1e02c
Shurafads <wypafads@gmail.com>
2022-10-17 21:22:20 +0300

    Добавляет функции для генерации объектов

--- js/main.js
+++ js/main.js
@@ -32,20 +32,79 @@ function getPositiveRandomNumber(from, to) {
   return getRandomIntInclusive(from, to);
 }
 
-const MIN_NUMBER = 5.3;
-const MAX_NUMBER = 5.3;
-
-getPositiveRandomNumber(MIN_NUMBER, MAX_NUMBER);
-
-console.log(getPositiveRandomNumber(MIN_NUMBER, MAX_NUMBER));
+getPositiveRandomNumber();
 
 //Функция для проверки максимальной длины строки.
 
-function checkMaxString(stroke, lenght) {
-  if (stroke <= lenght) {
-    return 'Слишком длинная строка';
-  }
-  return 'Ок';
-}
+// const checkStringLength = (string, length) => string.length <= length;
 
-checkMaxString(stroke, lenght);
+// checkStringLength();
+
+//Получаем рандомное число для id и url
+
+const INDEX_COUNT = 25;
+
+//Генерируем рандомное число лайков
+
+const likeCount = {
+  min: 15,
+  max: 200,
+};
+
+//Описание фотографии
+
+const photoDescription = [
+  'Расслабься и смотри',
+  'Кусочек самого лучшего путешествия',
+  'Неплохая локация для съемок',
+  'Просто оставлю это сдесь',
+  'Иногда хочется просто хочется такой же обстановки',
+  'Такие места вдохновляют',
+  'Были тут?',
+  'Об этом мечтает каждый',
+  'Чего бы это не стоило',
+  'У меня есть в запасе последний глоток тишины',
+  'Всегда новые локации!',
+  'Самый лучший день',
+  'Путешествовать и жить гораздо интереснее, если следуешь внезапно возникающим импульсам',
+  'На заставку',
+  'Тут бывает и не такое',
+  'Малая часть того, чем бы я хотел поделиться',
+  'Это самое потрясающее место!',
+  'Это не может быть правдой!',
+  'Такого в наших краях не увидишь',
+  'Без фильтра',
+  'Ещё одни тёплые выходные',
+  'Вся наша жизнь подчиняется природным ритмам',
+  'Есть в жизни счастье',
+  'Вперед исследовать!',
+  'Мое любимое место',
+];
+
+const getRandomElement = (elements) => elements[getRandomIntInclusive(0, elements.length - 1)];
+
+// Число комментариев
+
+const MAX_COMMENTS = 200;
+
+// Создание объекта
+
+const createPicture = (index) => ({
+  id: index,
+  url: `photos/${index}.jpg`,
+  description: getRandomElement(photoDescription),
+  likes: getRandomIntInclusive(likeCount.min, likeCount.max),
+  comments: getRandomIntInclusive(0, MAX_COMMENTS),
+});
+
+// Создание массива из объектов
+
+const getPictures = () => {
+  const pictures = [];
+  for (let i = 1; i <= INDEX_COUNT; i++) {
+    pictures.push(createPicture(i));
+  }
+  return pictures;
+};
+
+getPictures();
