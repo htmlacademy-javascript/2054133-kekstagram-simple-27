3173c57d9ac11fa1b6129eb2e99c0b2dbf9c6989
Shurafads <wypafads@gmail.com>
2022-11-06 20:01:37 +0300

    Добавляет редактирование масштаба изображения

--- js/picture-form.js
+++ js/picture-form.js
@@ -1,26 +1,26 @@
+import {onButtonSmallerClick, onButtonBiggerClick} from './picture-scale.js';
+
 const uploadFileInput = document.querySelector('#upload-file');
 const pageBody = document.querySelector('body');
 const modalPictureEditor = document.querySelector('.img-upload__overlay');
 const modalCloseButton = document.querySelector('#upload-cancel');
 const description = document.querySelector('.text__description');
 const pcitureForm = document.querySelector('.img-upload__form');
+const imageButtonScaleSmaller = document.querySelector('.scale__control--smaller');
+const imageButtonScaleBigger = document.querySelector('.scale__control--bigger');
 
-const pristine = new Pristine(pcitureForm,{
-  classTo: 'text',
-  errorTextParent: 'text',
-  errorTextClass: 'text__error',
-}, false);
-
-pcitureForm.addEventListener('submit', (evt) => {
-  if (!pristine.validate()) {
-    evt.preventDefault();
-  }
-});
+const pristine = new Pristine(pcitureForm,
+  {
+    classTo: 'text',
+    errorTextParent: 'text',
+  },
+  false);
 
 const closeModal = () => {
   modalPictureEditor.classList.add('hidden');
   pageBody.classList.remove('modal-open');
   pristine.reset();
+  document.removeEventListener('keydown', onModalEscKeyDown);
 };
 
 const openModal = () => {
@@ -29,17 +29,25 @@ const openModal = () => {
 };
 
 const isEscapeKey = (evt) => evt.key === 'Escape';
-const onModalEscKeyDown = (evt) => {
+function onModalEscKeyDown(evt) {
   if (isEscapeKey(evt)) {
     evt.preventDefault();
     closeModal();
+  }
+}
+
+const isValidate = (evt) => {
+  if (!pristine.validate()) {
+    evt.preventDefault();
   }
 };
 
 uploadFileInput.addEventListener('change', () => {
   openModal();
+  imageButtonScaleSmaller.addEventListener('click', onButtonSmallerClick);
+  imageButtonScaleBigger.addEventListener('click', onButtonBiggerClick);
   document.addEventListener('keydown', onModalEscKeyDown);
   modalCloseButton.addEventListener('click', closeModal);
+  description.addEventListener('keydown', (evt) => evt.stopPropagation());
+  pcitureForm.addEventListener('submit', isValidate);
 });
-
-description.addEventListener('keydown', (evt) => evt.stopPropagation());
---
+++ js/picture-scale.js
@@ -0,0 +1,31 @@
+const imageInputValue = document.querySelector('.scale__control--value');
+const imagePreview = document.querySelector('.img-upload__preview');
+
+const MIN_SCALE = 25;
+const MAX_SCALE = 100;
+const SCALE_STEP = 25;
+
+const scaleImage = (value) => {
+  imagePreview.style.transform = `scale(${value / 100})`;
+  imageInputValue.value = `${value}%`;
+};
+
+const onButtonSmallerClick = () => {
+  const currentValue = parseInt(imageInputValue.value, 10);
+  let newValue = currentValue;
+  if (newValue > MIN_SCALE) {
+    newValue -= SCALE_STEP;
+  }
+  scaleImage(newValue);
+};
+
+const onButtonBiggerClick = () => {
+  const currentValue = parseInt(imageInputValue.value, 10);
+  let newValue = currentValue;
+  if (newValue < MAX_SCALE) {
+    newValue += SCALE_STEP;
+  }
+  scaleImage(newValue);
+};
+
+export {onButtonSmallerClick, onButtonBiggerClick};
