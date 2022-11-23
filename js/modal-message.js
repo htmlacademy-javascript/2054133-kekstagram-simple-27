593f39dc8362d8a824fee8927202220fe38fc74e
Shurafads <wypafads@gmail.com>
2022-11-23 15:45:50 +0300

    Корректирует функцию проверки модального окна

--- js/modal-message.js
+++ js/modal-message.js
@@ -3,7 +3,11 @@ import {isEscapeKey} from './utils.js';
 const errorTemplate = document.querySelector('#error').content.querySelector('.error');
 const successTemplate = document.querySelector('#success').content.querySelector('.success');
 
-const isModalOn = (evt) => evt.target.classList.contains('success__button') || evt.target.classList.contains('error__button') || evt.target.classList.contains('error') || evt.target.classList.contains('success');
+const isModalOn = (evt) =>
+  evt.target.classList.contains('success__button') ||
+  evt.target.classList.contains('error__button') ||
+  evt.target.classList.contains('error') ||
+  evt.target.classList.contains('success');
 
 const renderModalMessage = (modalElement) => {
   document.body.append(modalElement);
